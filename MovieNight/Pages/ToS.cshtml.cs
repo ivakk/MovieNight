d74458d6ddc07079c641be1b3c390548bc3c9ab9
Ivaylo Chevarganov <i503708@fhict.nl>
2024-01-03 02:53:42 +0200

    Added web login and signup

--- MovieNight/Pages/ToS.cshtml.cs
+++ MovieNight/Pages/ToS.cshtml.cs
@@ -3,11 +3,10 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MovieNight.Pages
 {
-    public class RegisterModel : PageModel
+    public class ToSModel : PageModel
     {
         public void OnGet()
         {
-            
         }
     }
 }
